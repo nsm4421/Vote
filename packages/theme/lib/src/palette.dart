8b84770ce932d88ac3a65bf61b79848cb4088335
N <nsm4421@naver.com>
2025-11-27 06:52:28 +0900

    App Theme Package
    Light, Dak모드의 App Data 정의

---
+++ packages/theme/lib/src/palette.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppPalette {
+  const AppPalette._();
+
+  static const Color navy900 = Color(0xFF0D1B2A);
+  static const Color blue700 = Color(0xFF1565C0);
+  static const Color blue600 = Color(0xFF1E88E5);
+  static const Color teal500 = Color(0xFF2CB1BC);
+  static const Color lime400 = Color(0xFFB5E048);
+  static const Color orange400 = Color(0xFFF7A440);
+  static const Color red400 = Color(0xFFE74C3C);
+  static const Color gray50 = Color(0xFFF7F9FB);
+  static const Color gray100 = Color(0xFFEEF2F6);
+  static const Color gray300 = Color(0xFFD5DDE5);
+  static const Color gray600 = Color(0xFF6B7786);
+  static const Color gray700 = Color(0xFF425169);
+  static const Color gray800 = Color(0xFF1F2A3A);
+  static const Color gray900 = Color(0xFF0D1B2A);
+}
